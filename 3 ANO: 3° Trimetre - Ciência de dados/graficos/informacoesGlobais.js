21109f1d67e4fad3ce152dd5441ec20d89589f7f
ariedee <106112918+ariedee@users.noreply.github.com>
2024-10-02 08:07:20 -0300

    Add files via upload

--- 3 ANO: 3° Trimetre - Ciência de dados/graficos/informacoesGlobais.js
+++ 3 ANO: 3° Trimetre - Ciência de dados/graficos/informacoesGlobais.js
@@ -1,10 +1,16 @@
-const url = 'https://github.com/guilhermeonrails/api/blob/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     console.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Voce sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    console.log(paragrafo)
 }
 
-vizualizarInformacoesGlobais()
+const container = document.getElementById('graficos-container')
+container.appendChild(paragrafo)
 
+vizualizarInformacoesGlobais()
